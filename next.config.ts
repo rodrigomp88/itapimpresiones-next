976ecae396c7f33c50680db43757c432e7363376
rodrigomp88 <rodrigomp88@gmail.com>
2025-11-10 11:11:52 -0300

    resize images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/itap-shop.appspot.com/**",
+      },
+    ],
   },
 };
 
