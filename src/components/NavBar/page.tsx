74917fdc5d9bd3314008dca9cf55d451554bb381
Pablo Pinea <pablocatu@gmail.com>
2025-12-13 21:35:34 -0300

    Corrección de atributos SVG en Navbar

--- src/components/NavBar/page.tsx
+++ src/components/NavBar/page.tsx
@@ -55,8 +55,8 @@ const Navbar = () => {
           <Link href="/" className="flex items-center gap-3">
             <div className="size-8 text-blue-600">
               <svg fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_6_330)">
-                  <path clip-rule="evenodd" d="M24 0.757355L47.2426 24L24 47.2426L0.757355 24L24 0.757355ZM21 35.7574V12.2426L9.24264 24L21 35.7574Z" fill="currentColor" fill-rule="evenodd"></path>
+                <g clipPath="url(#clip0_6_330)">
+                  <path clipRule="evenodd" d="M24 0.757355L47.2426 24L24 47.2426L0.757355 24L24 0.757355ZM21 35.7574V12.2426L9.24264 24L21 35.7574Z" fill="currentColor" fillRule="evenodd"></path>
                 </g>
                 <defs>
                   <clipPath id="clip0_6_330">
